c6b334c1b33e2d1f787b5336d386338da2dea089
isalorsaw <isalorsaw@hotmail.com>
2021-10-27 20:38:38 -0600

    Agregar archivos de proyecto.

---
+++ Geolocalizacion/Geolocalizacion/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Xamarin.Essentials;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace Geolocalizacion
+{
+    public partial class MainPage : ContentPage
+    {
+        Position pos;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+
+                //double latt = 15.4;
+                //double longg = -86.9;
+
+                if (location != null)
+                {
+                    //Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    lblcoor.Text=($"Latitude: {location.Latitude}, Longitude: {location.Longitude}");
+                    pos = new Position(location.Latitude, location.Longitude);
+
+                    //Como Calcular la Distancia entre dos puntos
+                    double dis = location.CalculateDistance(location.Latitude, location.Longitude, DistanceUnits.Miles);
+                    DisplayAlert("Distancia", dis.ToString(), "OK");
+
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                //DisplayAlert("Error", fnsEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Mapa(pos));
+        }
+    }
+}
---
+++ Geolocalizacion/Geolocalizacion/Mapa.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace Geolocalizacion
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Mapa : ContentPage
+    {
+        public Mapa()
+        {
+            InitializeComponent();
+        }
+        public Mapa(Position p)
+        {
+            InitializeComponent();
+            DisplayLocal(p);
+        }
+
+        public void DisplayLocal(Position p)
+        {
+            var pin = new Pin
+            {
+                Type = PinType.Place,
+                Position = p,
+                Label = "Destination",
+                Address = "Lat: " + p.Latitude + ", Long: " + p.Longitude,
+            };
+            map.Pins.Add(pin);
+
+            MapSpan m = MapSpan.FromCenterAndRadius(p, Distance.FromMeters(800));
+            map.MoveToRegion(m);
+        }
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
